5fd1bc361bc92ccb4dcd79f3ceaf015b291ce406
golibjon <golibtoramurodov@gmail.com>
2025-07-07 00:22:19 +0900

    done

---
+++ 3_dart_ecosystem/task.dart
@@ -0,0 +1,15 @@
+//! I did it without creatingn flutter project, just dart project
+class Calculator {
+  double sum(double a, double b) => a + b;
+
+  double subtract(double a, double b) => a - b;
+
+  double multiply(double a, double b) => a * b;
+
+  double divide(double a, double b) {
+    if (b == 0) {
+      throw ArgumentError('Cannot divide by zero.');
+    }
+    return a / b;
+  }
+}
